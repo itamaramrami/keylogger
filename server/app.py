14d6ff566b04786234ff97b32ef03d4315b23635
eli <lykkh78@gmail.com>
2025-02-25 18:47:06 +0200

    שינוי קטן ולא מזיק

--- server/app.py
+++ server/app.py
@@ -1,13 +1,16 @@
 from flask import Flask
 from routes import app_routes
 
+from flask_cors import CORS
+
 
 
 app = Flask(__name__, static_folder="../front", static_url_path="")
+CORS(app)
 app.config['JSON_AS_ASCII'] = False
 app.register_blueprint(app_routes)
 
 
 
 if __name__ == "__main__":
-    app.run( port=5555, debug=True)
+    app.run(host='0.0.0.0', port=5555, debug=True)
